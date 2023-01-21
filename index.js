38bb4ac5280d742b5f38c0f30e7a7a7c30a071f6
rodionhatskevich <rodionhatskevich@gmail.com>
2023-01-21 20:36:26 +0100

    create base route

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req, res) => {
-    res.send('hi');
+app.all('/hello', (req, res, next) => {
+	console.log('All');
+	next();
 });
+
+const cb = (req, res, next) => {
+	console.log('CB');
+	next();
+}
+
+app.route('/user')
+	.get('/hello', (req, res) => {
+		res.send('Hi GET!');
+	})
+	.post('/hello', (req, res) => {
+		res.send('Hi Post!');
+	});
 
 app.listen(port, () => {
     console.log(`Server run on http://localhost:${port}`);
