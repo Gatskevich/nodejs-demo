b8bcc5ddd3dc46152d6eebfa757f40323a3a1b83
rodionhatskevich <rodionhatskevich@gmail.com>
2023-01-23 23:01:35 +0100

    add controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Response, Request } from "express";
+import { BaseController } from "../common/base.controller"
+import { LoggerService } from "../logger/logger.service"
+
+export class UserController extends BaseController {
+    constructor(logger: LoggerService) {
+        super(logger);
+        this.bindRoutes([{
+            path: '/register',
+            func: this.register,
+            method: 'post',
+        }, {
+            path: '/login',
+            func: this.login,
+            method: 'get',
+        }]);
+    }
+
+    login(req: Request, res: Response, next: NextFunction) {
+		this.ok(res, 'login');
+	}
+
+	register(req: Request, res: Response, next: NextFunction) {
+		this.ok(res, 'register');
+	}
+}
